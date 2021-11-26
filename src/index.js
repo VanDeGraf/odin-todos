703bddf4bf25199b63b6c1bed45284b6687f08f5
VanDeGraf <vandegraf@bk.ru>
2021-11-26 21:09:18 +0400

    Add general classes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
-console.log('test')
+import './stylesheet.css'
+
+class CheckListItem {
+    constructor(title, checked = false) {
+        this.title = title
+        this.checked = checked
+    }
+}
+
+class TodoItem {
+    constructor(title,
+                description = '',
+                dueDate = null,
+                priority = 0,
+                checkListItems = null) {
+        this.title = title
+        this.description = description
+        this.dueDate = dueDate
+        this.priority = priority
+        this.checkListItems = checkListItems === null ? [] : checkListItems
+        this.completed = false
+    }
+}
+
+class Project {
+    constructor(title) {
+        this.title = title
+        this.todoItems = []
+    }
+}
